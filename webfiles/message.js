a81be675b4745761c64535085435c32ad0ddbd9e
Zainab Ali <77604160+zaali33@users.noreply.github.com>
2021-04-07 20:21:17 +0300

    remove 2 insert statements

--- webfiles/message.js
+++ webfiles/message.js
@@ -64,7 +64,7 @@ class MessageManager {
     async sendMessage(message) {
         await this.createConnection()
         await this.query(this.dbcon, `insert into message (message, receiverid, senderid, date) values('${message.message}', '${message.receiverid}', '${message.senderid}', now());`)
-        await this.query(this.dbcon, `insert into message (message, receiverid, senderid, date) values('${message.message}', '${message.receiverid}', '${message.senderid}', now());`)
+    
     }
 
     async checkMessage(message) {
